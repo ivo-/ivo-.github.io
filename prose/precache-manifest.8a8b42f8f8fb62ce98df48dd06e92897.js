3436a230ac57fe979ecbede7c9116e77e51c4823
ivo- <ivailo.nikolaev.hristov@gmail.com>
2020-01-06 11:43:49 +0200

    Deploy Prose

---
+++ prose/precache-manifest.8a8b42f8f8fb62ce98df48dd06e92897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4879e7f3ccfe7e68ec",
+    "url": "/prose/static/js/runtime~main.0a4db5c6.js"
+  },
+  {
+    "revision": "c5358488712bd3444601",
+    "url": "/prose/static/js/main.b6cd6afd.chunk.js"
+  },
+  {
+    "revision": "e4b40e0ff7a72529fc02",
+    "url": "/prose/static/js/2.6daa9d0b.chunk.js"
+  },
+  {
+    "revision": "c5358488712bd3444601",
+    "url": "/prose/static/css/main.6874d937.chunk.css"
+  },
+  {
+    "revision": "e4b40e0ff7a72529fc02",
+    "url": "/prose/static/css/2.f2432de9.chunk.css"
+  },
+  {
+    "revision": "976b49f59cd2d926c60ac73299c44a37",
+    "url": "/prose/index.html"
+  }
+];
--- prose/service-worker.js
+++ prose/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b45552dcc3072469fe2ac60d48502e06.js"
+  "/prose/precache-manifest.8a8b42f8f8fb62ce98df48dd06e92897.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/prose/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
